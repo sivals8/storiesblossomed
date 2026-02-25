77873bff0d3c90dabb5eb64cc6f4729841349101
sivals8 <sivals8@mcmaster.ca>
2026-02-25 00:34:04 -0500

    Fixing scroll to top issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./sections/About"; // can be used inside Home or separately
 import Stories from "./sections/Stories";
 import Contact from "./sections/Contact"; // same as above
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";     // new page
@@ -12,34 +13,37 @@ import ContactPage from "./pages/ContactPage"; // new page
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        {/* Home route */}
-        <Route
-          path="/"
-          element={
-            <>
-              <div className="container mx-auto max-w-7xl" path= "/">
-                <Hero />
-              </div>
-              <div id = "stories">
-                <Stories />
-              </div>
-              
-            </>   
-          }
-        />
+    <>
+      <Router>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          {/* Home route */}
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="container mx-auto max-w-7xl" path= "/">
+                  <Hero />
+                </div>
+                <div id = "stories">
+                  <Stories />
+                </div>
+                
+              </>   
+            }
+          />
 
-        {/* About page */}
-        <Route path="/about" element={<About />} />
+          {/* About page */}
+          <Route path="/about" element={<About />} />
 
-        {/* Contact page */}
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+          {/* Contact page */}
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
 
-      <Footer />
-    </Router>
+        <Footer />
+      </Router>
+    </>
   );
 };
 
